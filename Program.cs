db664a1567c6e76400c9a6edd961fc971236c7e1
otmane.aznag <otmane.aznag@gmail.comgit config --global user.email otmane.aznag@gmail.comgit config --global user.email otmane.aznag@gmail.com>
2021-01-15 22:35:24 +0100

    add json formating to output logging

--- Program.cs
+++ Program.cs
@@ -18,6 +18,12 @@ namespace HelloDotNet5
 
         public static IHostBuilder CreateHostBuilder(string[] args) =>
             Host.CreateDefaultBuilder(args)
+                .ConfigureLogging((context, logging)=>{
+                    if(context.HostingEnvironment.IsProduction()){
+                        logging.ClearProviders();
+                        logging.AddJsonConsole();  
+                    }
+                })
                 .ConfigureWebHostDefaults(webBuilder =>
                 {
                     webBuilder.UseStartup<Startup>();
